dade984f77878d595e008bac74df449c8cb4d2cd
tsfernando2003 <tsfernando2003@gmail.com>
2025-10-03 07:52:36 +0530

    add userDashBoard page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,32 @@
 import React from "react";
-import { FaHome, FaInfoCircle, FaEnvelope , FaHandHoldingMedical } from "react-icons/fa";
+import { FaHome, FaInfoCircle, FaEnvelope, FaHandHoldingMedical } from "react-icons/fa";
+import { Link } from "react-router-dom"; // <-- import Link
 import "./Header.css";
 
 const Header: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-gradient fixed-top header">
       <div className="container-fluid">
-          <a className="navbar-brand fw-bold" href="#">
+        <Link className="navbar-brand fw-bold" to="/">
           <FaHandHoldingMedical className="me-2 ms-1" /> CareNet
-        </a>
+        </Link>
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" href="#">
+              <Link className="nav-link d-flex align-items-center" to="/">
                 <FaHome className="me-1" /> Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" href="#">
+              <Link className="nav-link d-flex align-items-center" to="/dashboard">
                 <FaInfoCircle className="me-1" /> About
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" href="#">
+              <Link className="nav-link d-flex align-items-center" to="/contact">
                 <FaEnvelope className="me-1" /> Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
